55f54d9d1c925cdb15dc69c5fb535c718e3b8c03
Claire Leconte <claire.lcnt@gmail.com>
2021-06-22 23:54:00 +0200

    First commit, display basic html & axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+  const [isLoading, setIsLoading] = useState(false);
+  const [users, setUsers] = useState();
+  const [search, setSearch] = useState("");
+
+  const handleOnChange = (event) => {
+    //debounce
+    //const search = event.target.value;
+    setSearch(event.target.value);
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.github.com/search/users?q=${search}`
+        );
+        if (response.status === 200) {
+          setUsers(response.data.items);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  };
+
+  console.log("users : ", users);
+
+  return isLoading ? (
+    <p>wait a minute... ;-)</p>
+  ) : (
+    <>
+      <h1>Github User Search</h1>
+      <input
+        type="search"
+        id="user-search"
+        name="user-search"
+        value={search}
+        onChange={handleOnChange}
+      />
+
+      {users &&
+        users.map((user, index) => {
+          return <p key={index}>{user.login}</p>;
+        })}
+    </>
   );
 }
 
